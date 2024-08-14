6452c4bf536499b3b903a7f6d07820853dbf9ec5
sadiqaliarsalan <sadiqaliarsalan@gmail.com>
2024-08-14 17:04:53 +0200

    Added unit tests for customer and order manager

---
+++ WebPizzaCommon.Tests/Managers/OrderManagerTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebPizzaCommon.Managers;
+
+namespace WebPizzaCommon.Tests.Managers
+{
+    [TestFixture]
+    public class OrderManagerTests
+    {
+        private OrderManager _orderManager;
+
+        [SetUp]
+        public void Setup()
+        {
+            _orderManager = new OrderManager();
+        }
+
+        [Test]
+        public void IncrementOrder_ShouldIncreaseOrderCount_WhenNewCustomer_Test()
+        {
+            var customerId = "some customer id";
+            _orderManager.IncrementOrder(customerId);
+            _orderManager.IncrementOrder(customerId);
+            _orderManager.IncrementOrder(customerId);
+            _orderManager.IncrementOrder(customerId);
+            _orderManager.IncrementOrder(customerId);
+
+            Assert.AreEqual(5, _orderManager.GetOrderCount(customerId));
+        }
+
+        [Test]
+        public void DecrementOrder_ShouldDecreaseOrderCount_WhenOrdersExist_Test()
+        {
+            var customerId = "some customer id";
+            _orderManager.IncrementOrder(customerId);
+            _orderManager.IncrementOrder(customerId);
+            _orderManager.DecrementOrder(customerId);
+
+            Assert.AreEqual(1, _orderManager.GetOrderCount(customerId));
+        }
+
+        [Test]
+        public void CompleteOrder_ShouldTransferOrderToCompleted_WhenOrdersExist_Test()
+        {
+            var customerId = "some customer id";
+            _orderManager.IncrementOrder(customerId);
+            _orderManager.CompleteOrder(customerId);
+
+            Assert.AreEqual(0, _orderManager.GetOrderCount(customerId));
+            Assert.AreEqual(1, _orderManager.GetCompletedOrderCount(customerId));
+        }
+
+        [Test]
+        public void CompleteOrder_ShouldReturnZero_WhenNoOrdersExist_Test()
+        {
+            var customerId = "some customer id";
+            _orderManager.CompleteOrder(customerId);
+
+            Assert.AreEqual(0, _orderManager.GetOrderCount(customerId));
+            Assert.AreEqual(0, _orderManager.GetCompletedOrderCount(customerId));
+        }
+    }
+}
